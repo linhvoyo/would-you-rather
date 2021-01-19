29c25de93ae690b7c727a0927fdc6046c2c5d3d4
linhvoyo <linhvoyo@ymail.com>
2021-01-19 06:27:19 -0800

    chore: Add Bulma
    Setup Bulma css framework

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
+
 import './App.css';
 import { initApp } from '../actions';
 
@@ -12,12 +15,17 @@ class App extends React.Component {
   }
 
   render() {
-    return <div className="App">App</div>;
+    return (
+      <div className="App">
+        App
+        <Button color="primary">My Bulma button</Button>
+      </div>
+    );
   }
 }
+
+export default connect()(App);
 
 App.propTypes = {
   dispatch: PropTypes.func.isRequired,
 };
-
-export default connect()(App);
